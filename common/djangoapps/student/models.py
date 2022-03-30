19f65d5f51047aaf3090d56a3275fa6a0ea03b72
Kheav Samnang <khievsamnang957@gmail.com>
2022-03-30 21:08:08 +0700

    Fix ForeignKey mis-use that cause error when run migration

--- common/djangoapps/student/models.py
+++ common/djangoapps/student/models.py
@@ -508,7 +508,7 @@ class UserProfile(models.Model):
     # Sanitize all fields.
     # This is not visible to other users, but could introduce holes later
     # Add branch field to link student to specific branch
-    branch = models.ForeignKey(Branch, db_index=True, on_delete=models.PROTECT, blank=True, null=True)
+    branch = models.ForeignKey('branch.Branch', db_index=True, on_delete=models.PROTECT, blank=True, null=True)
     user = models.OneToOneField(User, unique=True, db_index=True, related_name='profile', on_delete=models.CASCADE)
     name = models.CharField(blank=True, max_length=255, db_index=True)
 
